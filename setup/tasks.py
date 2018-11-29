a706e032d49787896ad26d04a32ba69b73197336
flix <lxd-best@163.com>
2018-11-29 16:44:28 +0800

    change scp user from root to admin and add script suffix

--- setup/tasks.py
+++ setup/tasks.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from subprocess import Popen, PIPE
 from cmdb.models import Host
-import sh
+import sh, os
 from config.views import get_dir
 scripts_dir = get_dir("s_path")
 
@@ -20,8 +20,14 @@ def command(host, name):
 @shared_task
 def script(host, name):
     h = Host.objects.get(hostname=host)
-    sh.scp(scripts_dir+name, "root@"+h.ip+":/tmp/"+name)
-    cmd = "ssh root@"+h.ip+" "+'"sh /tmp/{}"'.format(name)
+    sh.scp(scripts_dir+name, "admin@"+h.ip+":/tmp/"+name)
+    file_suffix = os.path.splitext(name)[1]
+    cmd = "ssh admin@" + h.ip + " " + '"sh /tmp/{}"'.format(name)
+    if file_suffix == '.sh':
+        cmd = "ssh admin@" + h.ip + " " + '"sh /tmp/{}"'.format(name)
+    elif file_suffix == '.py':
+        cmd = "ssh admin@" + h.ip + " " + '"python /tmp/{}"'.format(name)
     p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     data = p.communicate()
     return data
+
